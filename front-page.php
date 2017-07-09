80aa88a765475b0a26f9c4f23074eb50a04081f0
Jake <mubok0212@gmail.com>
2017-07-09 20:59:58 +0900

    fontello

--- front-page.php
+++ front-page.php
@@ -1,8 +1,14 @@
 <?php get_header( 'home' ); ?>
-    <div class="portfolio">
 
-        <h2 class="text-center">포트폴리오</h2>
-        <div class="bottom-line"></div>
+      <div class="portfolio-outter-box">
+        <div class="portfolio-box">
+          <h2>포트폴리오</h2>
+          <h5>사이트 모음</h5>
+        </div>
+      </div>
+    <article class="portfolio">
+
+        <?php for( $i =0; $i < 6; $i++ ) :?>
 
         <div class="col-sm-6 col-md-4">
           <div class="thumbnail">
@@ -14,57 +20,10 @@
             </div>
           </div>
         </div>
-        <div class="col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="..." alt="...">
-            <div class="caption">
-              <h3>Thumbnail label</h3>
-              <p>...</p>
-              <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="..." alt="...">
-            <div class="caption">
-              <h3>Thumbnail label</h3>
-              <p>...</p>
-              <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="..." alt="...">
-            <div class="caption">
-              <h3>Thumbnail label</h3>
-              <p>...</p>
-              <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="..." alt="...">
-            <div class="caption">
-              <h3>Thumbnail label</h3>
-              <p>...</p>
-              <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-          <div class="thumbnail">
-            <img src="..." alt="...">
-            <div class="caption">
-              <h3>Thumbnail label</h3>
-              <p>...</p>
-              <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-            </div>
-          </div>
-        </div>
-    </div>
+
+        <?php endfor; ?>
+
+    </article>
 
     <div class="video">
       <h2 class="text-center">동영상</h2>
