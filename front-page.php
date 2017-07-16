2ec422b8df4e4970e9af397930b00ebbdeef7531
Jake <mubok0212@gmail.com>
2017-07-16 10:17:05 +0900

    front-page building

--- front-page.php
+++ front-page.php
@@ -9,12 +9,37 @@
         </div>
         <p>워드프레스에 대한 영상과 설명이 있습니다.</p>
       </div>
-      <div class="col-sm-12 col-md-6">
-        <div class="left">
-          <?php echo file_get_contents( "./wp-content/themes/my-theme/img/laptop-icon.svg" ); ?>
+      <div class="video row">
+        <div class="col-sm-12 col-md-6 col-lg-6">
+          <div class="left">
+            <img src="./wp-content/themes/my-theme/img/laptop-com.svg" alt="">
+          </div>
         </div>
-      </div>
-      <div class="col-sm-12 col-md-6">
+        <div class="col-sm-12 col-md-6 col-lg-6">
+          <div class="right">
+            <div class="title-box">
+              <h4>워드프레스 강좌</h4>
+              <div class="main-title">
+                <h2>워드프레스 홈페이지 1시간안에 만들기</h2>
+              </div>
+                <p>
+                  난이도   <span class="p-separator">|</span>   초급<br>
+                  영상개수   <span class="p-separator">|</span>   1개<br>
+                  영상시간   <span class="p-separator">|</span>   1:20<br>
+                  워드프레스 초보자분들도 무리없이 따라하시며<br>
+                  만들 수 있습니다.<br>
+                  홈페이지를 같이 만들어가며 배우는 영상입니다.
+                </p>
+            </div>
+              <a class="video-link" href="#">강좌 보기</a>
+          </div>
+        </div>
+
+        <div class="selector col-sm-12 col-md-12 col-lg-12">
+            <?php for( $i =0; $i < 3; $i++ ) :?>
+                <div class="video-button"></div>
+            <?php endfor; ?>
+        </div>
 
       </div>
     </article>
